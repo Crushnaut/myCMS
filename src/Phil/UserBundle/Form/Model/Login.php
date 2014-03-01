9e6e8c993a0e3ace342b30f32a4cd28ead478bdb
Phil Mowatt <omegared@gmail.com>
2014-03-01 03:43:25 +0000

    minor

---
+++ src/Phil/UserBundle/Form/Model/Login.php
@@ -0,0 +1,12 @@
+<?php
+// src/Phil/UserBundle/Form/Model/Login.php
+namespace Phil\UserBundle\Form\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+//use Phil\UserBundle\Entity\User;
+
+class login
+{
+
+}
