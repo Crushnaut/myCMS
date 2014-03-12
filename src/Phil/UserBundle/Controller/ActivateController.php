6324690da1851b61e242fcf3e17bffd792c6bb5c
Phil Mowatt <omegared@gmail.com>
2014-03-12 22:40:21 +0000

    Phase 2 - Cleaned up activation controller and eliminated nested ifs.

--- src/Phil/UserBundle/Controller/ActivateController.php
+++ src/Phil/UserBundle/Controller/ActivateController.php
@@ -20,7 +20,6 @@ class ActivateController extends Controller
      */
     public function activateAction($activationCode = null, Request $request)
     {
-
         $form = $this->createForm(new ActivationType(), new Activation());
         $form->handleRequest($request);
 
@@ -29,30 +28,30 @@ class ActivateController extends Controller
             $activationCode = $form->getData()->getActivationCode();
         }
 
-        if ($activationCode !== null)
+        // if a activation code is not specified in the URL or by the form, display the form
+        if (is_null($activationCode))
         {
-            $em = $this->getDoctrine()->getManager();
-            $user = $em->getRepository('PhilUserBundle:User')->findOneByActivationCode($activationCode);
-
-            if (is_null($user) === true)
-            {   
-                $this->get('session')->getFlashBag()->add('notice', "That is not a valid activation code.");
-            }
-
-            if (false === $user->isEnabled())
-            {   
-                $user->activate();
-                $em->persist($user);
-                $em->flush();
-
-                $this->get('session')->getFlashBag()->add('notice', "" . $user->getUsername() ." has been activated. Please login.");
-
-                return $this->redirect($this->generateUrl('login'));
-            }
+            return $this->render('PhilUserBundle:Activate:activateForm.html.twig', array('form' => $form->createView()));
         }
 
-        return $this->render('PhilUserBundle:Activate:activateForm.html.twig', array('form' => $form->createView()));
-    }
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('PhilUserBundle:User')->findOneByActivationCode($activationCode);
+
+        // if a user does not match up with an activation code then the code isn't valid
+        if (is_null($user))
+        {   
+            $this->get('session')->getFlashBag()->add('notice', "That is not a valid activation code.");
+        }
+
+        // if we pass all these checks then we can activate the user
+        $user->activate();
+        $em->persist($user);
+        $em->flush();
+
+        $this->get('session')->getFlashBag()->add('notice', "" . $user->getUsername() ." has been activated. Please login.");
+
+        return $this->redirect($this->generateUrl('login'));
+}
     
     /*
      *  Action called when the resend activation code form is viewed or submitted.
@@ -62,30 +61,36 @@ class ActivateController extends Controller
         $form = $this->createForm(new EmailType(), new Email());
         $form->handleRequest($request);
 
-        if ($form->isValid())
+        // if the form is not valid or there are errors display the form
+        if ($form->isValid() === false)
         {
-            $em = $this->getDoctrine()->getManager();
-            $user = $em->getRepository('PhilUserBundle:User')->findOneByEmail($form->getData()->getEmail());
-
-            if (is_null($user) === false)
-            {
-                if ($user->isEnabled())
-                {
-                    $this->get('session')->getFlashBag()->add('notice', 'That user has already been activated.');
-                    $title = "Resend Activation Code";
-                    return $this->render('PhilUserBundle:User:emailForm.html.twig', array('form' => $form->createView(), 'title' => $title));
-                }
-                
-                $this->sendActivationEmail($user);
-                $this->get('session')->getFlashBag()->add('notice', 'You have been sent an e-mail containing a code to activate your account. Enter it below before you can login.');
-                return $this->redirect($this->generateUrl('user_activate', array('userID' => $user->getID())));
-            } 
-
-            $this->get('session')->getFlashBag()->add('notice', 'Specified e-mail address does not correspond with a user in our database.');
+            $title = "Resend Activation Code";
+            return $this->render('PhilUserBundle:User:emailForm.html.twig', array('form' => $form->createView(), 'title' => $title));
         }
 
-        $title = "Resend Activation Code";
-        return $this->render('PhilUserBundle:User:emailForm.html.twig', array('form' => $form->createView(), 'title' => $title));
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('PhilUserBundle:User')->findOneByEmail($form->getData()->getEmail());
+
+        // if an email does not match a user then the email is not valid
+        if (is_null($user))
+        {
+            $this->get('session')->getFlashBag()->add('notice', 'Specified e-mail address does not correspond with a user in our database.');
+            $title = "Resend Activation Code";
+            return $this->render('PhilUserBundle:User:emailForm.html.twig', array('form' => $form->createView(), 'title' => $title));
+        }
+
+        // if the user is enabled then there is no need to send an activation email
+        if ($user->isEnabled())
+        {
+            $this->get('session')->getFlashBag()->add('notice', 'That user has already been activated.');
+            $title = "Resend Activation Code";
+            return $this->render('PhilUserBundle:User:emailForm.html.twig', array('form' => $form->createView(), 'title' => $title));
+        }
+        
+        // if we pass all the above tests we can send the activation email
+        $this->sendActivationEmail($user);
+        $this->get('session')->getFlashBag()->add('notice', 'You have been sent an e-mail containing a code to activate your account. Enter it below before you can login.');
+        return $this->redirect($this->generateUrl('user_activate', array('userID' => $user->getID())));
     }
 
     /*
